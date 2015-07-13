ada6a29e8b02b7f854855078eb319667ab026c9f
Kamil <altaran007@gmail.com>
2015-07-13 12:02:23 +0200

    Akt2

--- main.c
+++ main.c
@@ -17,18 +17,18 @@ void* foo(void *arg){
         pthread_cond_wait(&COND,&MTX);
     printf("Can printf \n");
     pthread_mutex_unlock(&MTX);
-    return 0;
    }
+   return 0;
 }
 
 int main(int c, char** u){
-    pthread_t = 0;
+    pthread_t t = 0;
     pthread_create(&t,0,foo,0);
     sleep(5);
     pthread_mutex_lock(&MTX);
     PRINT = true;
     pthread_cond_signal(&COND);
     pthread_mutex_unlock(&MTX);
-    pthread_join(t);
+    pthread_join(t,NULL);
     return 0;
 }
