6b717050ce273e095bc65f298a19e6aff069687b
Kamil <altaran007@gmail.com>
2015-07-13 14:37:51 +0200

    Akt2

--- main.c
+++ main.c
@@ -7,28 +7,49 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+int turn = 0;
 pthread_mutex_t MTX = PTHREAD_MUTEX_INITIALIZER ;
 pthread_cond_t COND = PTHREAD_COND_INITIALIZER ;
-bool PRINT = false;
+bool PRINT = true;
 
 void* foo(void *arg){
-   pthread_mutex_lock(&MTX);
-   while( !PRINT ){
-        pthread_cond_wait(&COND,&MTX);
-    printf("Can printf \n");
-    pthread_mutex_unlock(&MTX);
-   }
+    while(turn){
+       pthread_mutex_lock(&MTX);
+       while( !PRINT )
+            pthread_cond_wait(&COND,&MTX);
+
+        printf("PING \n");
+        PRINT = false;
+        pthread_cond_signal(&COND);
+        pthread_mutex_unlock(&MTX);
+    }
    return 0;
 }
+void* foo2(void *arg){
+    while(turn--){
+        pthread_mutex_lock(&MTX);
+        while( PRINT )
+            pthread_cond_wait(&COND,&MTX);
 
-int main(int c, char** u){
+        printf("PONG \n");
+        PRINT = true;
+        pthread_cond_signal(&COND);
+        pthread_mutex_unlock(&MTX);
+    }
+    return 0;
+}
+
+int main(int argc, char ** argv){
     pthread_t t = 0;
+    pthread_t s = 0;
+
+    turn = atoi(argv[1]);
+
     pthread_create(&t,0,foo,0);
-    sleep(5);
-    pthread_mutex_lock(&MTX);
-    PRINT = true;
-    pthread_cond_signal(&COND);
-    pthread_mutex_unlock(&MTX);
+    pthread_create(&s,0,foo2,0);
+
     pthread_join(t,NULL);
+    pthread_join(s,NULL);
+
     return 0;
 }
